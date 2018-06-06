1be27604290e2b8cf17320ed2253b4540c1f66ab
yammouch <yammouch@gmail.com>
2018-06-06 12:20:21 +0900

    First checkin.

---
+++ sva/ex04.sv
@@ -0,0 +1,29 @@
+module ex02;
+
+  logic rstx0, rstx1;
+  logic d0;
+
+  property p0;
+    //@(rstx0 or rstx1 or d0) disable iff (!rstx0 || !rstx1) d0;
+    @(rstx0 or rstx1 or d0) /*disable iff (!rstx0 || !rstx1)*/ d0;
+  endproperty
+
+  initial begin
+    {rstx0, rstx1, d0} = 3'b000; #1ms;
+    {rstx0, rstx1, d0} = 3'b010; #1ms;
+    {rstx0, rstx1, d0} = 3'b000; #1ms;
+    {rstx0, rstx1, d0} = 3'b100; #1ms;
+    {rstx0, rstx1, d0} = 3'b000; #1ms;
+    {rstx0, rstx1, d0} = 3'b100; #1ms;
+    {rstx0, rstx1, d0} = 3'b110; #1ms;
+    {rstx0, rstx1, d0} = 3'b100; #1ms;
+    {rstx0, rstx1, d0} = 3'b101; #1ms;
+    {rstx0, rstx1, d0} = 3'b111; #1ms;
+    {rstx0, rstx1, d0} = 3'b101; #1ms;
+    {rstx0, rstx1, d0} = 3'b111; #1ms;
+    {rstx0, rstx1, d0} = 3'b110; #1ms;
+    {rstx0, rstx1, d0} = 3'b111; #1ms;
+    {rstx0, rstx1, d0} = 3'b101; #1ms;
+  end
+
+endmodule
