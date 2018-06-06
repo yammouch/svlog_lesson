7d67a24eb6205324232e91f850c84d9ea5beab8f
yammouch <yammouch@gmail.com>
2018-06-06 11:54:36 +0900

    First checkin.

---
+++ sva/ex03.sv
@@ -0,0 +1,23 @@
+module ex02;
+
+  logic d0, d1;
+
+  property p0;
+    @(d0 or d1) d0 == d1;
+  endproperty
+
+  assert property (p0);
+
+  task test1(input [1:0] data);
+    {d1, d0} = data;
+    #1ms;
+  endtask
+
+  initial begin
+    test1(2'b00);
+    test1(2'b01);
+    test1(2'b11);
+    test1(2'b10);
+  end
+
+endmodule
