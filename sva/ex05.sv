3addc4dfc0ed9609c7c628c9b5d63e0ac031468b
yammouch <yammouch@gmail.com>
2018-06-06 12:22:05 +0900

    First checkin.

---
+++ sva/ex05.sv
@@ -0,0 +1,16 @@
+module ex02;
+
+  logic d0;
+
+  property p0;
+    @(d0) d0 == 1'b1;
+  endproperty
+
+  initial begin
+    d0 = 1'b0; #1ms;
+    d0 = 1'b1; #1ms;
+    d0 = 1'b0; #1ms;
+    d0 = 1'b1; #1ms;
+  end
+
+endmodule
